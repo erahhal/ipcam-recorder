c63238c6380bb491c5762d6d2526688aaada9ec4
Ellis Rahhal <ellis.rahhal@gmail.com>
2020-04-11 14:27:55 -0700

    added date-based folders; now managing stdout and stderr

--- record.py
+++ record.py
@@ -5,10 +5,13 @@ import datetime
 import glob
 import multiprocessing
 import os
+import pathlib
+import queue
 import re
 import signal
 import subprocess
 import sys
+import threading
 import time
 
 # Switch to script path
@@ -19,7 +22,8 @@ os.chdir(dname)
 RECORDING_PATH = os.getcwd()
 RECORDING_LENGTH_S = 300
 MIN_FREE_DISK_KB = 200000
-DATE_FORMAT = '%Y-%m-%d_%H-%M-%S'
+FOLDER_DATE_FORMAT = '%Y-%m-%d'
+FILE_DATE_FORMAT = '%Y-%m-%d_%H-%M-%S'
 
 """
 Config file format:
@@ -38,7 +42,7 @@ def get_free_space(path):
     return available
 
 def record_stream(name, url):
-    file_name = '{}_{}.mp4'.format(name, DATE_FORMAT)
+    file_name = '{}/{}_{}.mp4'.format(FOLDER_DATE_FORMAT, name, FILE_DATE_FORMAT)
     recording_length = str(RECORDING_LENGTH_S)
     args = ['nohup',
             'ffmpeg',
@@ -70,27 +74,84 @@ def record_stream(name, url):
             # behind an uncorrupted video, but not enough
             # data to know if this is actually having an effect
             os.setpgrp()
-        p = subprocess.Popen(args, preexec_fn=preexec)
+        p = subprocess.Popen(args,
+                             preexec_fn=preexec,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             stdin=subprocess.DEVNULL,
+                             universal_newlines=True)
         def sigterm_handler(signum, frame):
             os.kill(p.pid, signal.SIGINT)
         signal.signal(signal.SIGTERM, sigterm_handler)
-        p.wait()
+
+        # Manage output using threads
+        def enqueue_output(out, queue):
+            for line in iter(out.readline, b''):
+                queue.put(line)
+            out.close()
+        q_stdout = queue.Queue()
+        t_stdout = threading.Thread(target=enqueue_output, args=(p.stdout, q_stdout))
+        t_stdout.daemon = True
+        t_stdout.start()
+        q_stderr = queue.Queue()
+        t_stderr = threading.Thread(target=enqueue_output, args=(p.stderr, q_stderr))
+        t_stderr.daemon = True
+        t_stderr.start()
+
+        running = True
+        while running:
+            try:
+                line = q_stdout.get_nowait()
+                # Could print to a log
+            except queue.Empty:
+                # no output
+                pass
+
+            try:
+                line = q_stderr.get_nowait()
+                # Could print to a log
+            except queue.Empty:
+                # no output
+                pass
+
+            try:
+                p.wait(timeout=3)
+                running = False
+            except subprocess.TimeoutExpired:
+                # still running
+                pass
     except KeyboardInterrupt:
         print('Interrupt in process ' + name)
         os.kill(p.pid, signal.SIGINT)
     finally:
         print('Cleaning up in process ' + name)
 
+def monitor_folders():
+    try:
+        while True:
+            now = datetime.datetime.today()
+            folder_name = now.strftime('%Y-%m-%d')
+            pathlib.Path(folder_name).mkdir(parents=True, exist_ok=True)
+            if now.hour == 23 and now.minute > 54:
+                # create folder for next day before ffmpeg has a chance to write
+                next_day = now + datetime.timedelta(days=1)
+                folder_name_next_day = next_day.strftime('%Y-%m-%d')
+                pathlib.Path(folder_name_next_day).mkdir(parents=True, exist_ok=True)
+            time.sleep(5)
+    except KeyboardInterrupt:
+        print('Interrupt in disk monitor process')
+    finally:
+        print('Cleaning up disk monitor process')
 
 def get_oldest_recording():
     p = re.compile('^.+_(\\d{4}-\\d{2}-\\d{2}_\\d{2}-\\d{2}-\\d{2}).mp4')
     oldest_filename = None
     oldest_timestamp = 9999999999999
-    for filename in glob.glob('*.mp4'):
+    for filename in glob.glob('**/*.mp4'):
         result = p.search(filename)
         if result is not None:
             date_str = result.group(1)
-            timestamp = time.mktime(datetime.datetime.strptime(date_str, DATE_FORMAT).timetuple())
+            timestamp = time.mktime(datetime.datetime.strptime(date_str, FILE_DATE_FORMAT).timetuple())
             if timestamp < oldest_timestamp:
                 oldest_timestamp = timestamp
                 oldest_filename = filename
@@ -145,7 +206,10 @@ def main(argv):
             cameras[name] = url
     p = multiprocessing.Process(target=monitor_disk_space)
     p.start()
-    processes[p.sentinel] = 'monitor'
+    processes[p.sentinel] = 'monitor_disk_space'
+    p = multiprocessing.Process(target=monitor_folders)
+    p.start()
+    processes[p.sentinel] = 'monitor_folders'
     for name, url in cameras.items():
         p = multiprocessing.Process(target=record_stream, args=(name, url))
         p.start()
@@ -157,9 +221,14 @@ def main(argv):
             sentinels = processes.keys()
             exited_sentinels = wait(sentinels)
             for sentinel in exited_sentinels:
-                if processes[sentinel] == 'monitor':
+                if processes[sentinel] == 'monitor_disk_space':
                     del processes[sentinel]
                     p = multiprocessing.Process(target=monitor_disk_space)
+                    p.start()
+                    processes[p.sentinel] = 'monitor'
+                elif processes[sentinel] == 'monitor_folders':
+                    del processes[sentinel]
+                    p = multiprocessing.Process(target=monitor_folders)
                     p.start()
                     processes[p.sentinel] = 'monitor'
                 else:
